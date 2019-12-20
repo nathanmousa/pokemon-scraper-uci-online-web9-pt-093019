db465bd021c4140518373b451e46ef942616d5e2
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-20 00:09:25 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      SQL
+    db.execute(sql, name, type)
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+      SQL
+    obj = db.execute(sql, id)
+    Pokemon.new(obj[0], obj[1], obj[2], db)
+  end
 end

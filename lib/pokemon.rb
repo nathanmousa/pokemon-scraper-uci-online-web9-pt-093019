491bda1e6cba34b4837d8a5ce2ca9a8716b27fb2
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-20 00:12:31 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,22 +1,29 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
-
-  def initialize(id:, name:, type:, db:)
-    @id, @name, @type, @db = id, name, type, db
+  
+  def initialize(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
-
+  
   def self.save(name, type, db)
     sql = <<-SQL
-      INSERT INTO pokemon(name, type) VALUES(?,?)
-    SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      SQL
     db.execute(sql, name, type)
   end
-
+  
   def self.find(id, db)
     sql = <<-SQL
-      SELECT * FROM pokemon WHERE id = ?
-    SQL
-    pokemon_row = db.execute(sql, id)[0]
-    new_pokemon = Pokemon.new(:id => pokemon_row[0], :name => pokemon_row[1], :type => pokemon_row[2], :db => db)
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+      LIMIT 1
+      SQL
+    obj = db.execute(sql, id)
+    pokemon = Pokemon.new(obj[0], obj[1], obj[2], db)
   end
 end
